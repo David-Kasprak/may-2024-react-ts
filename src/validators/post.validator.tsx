2a8b45588012c63b6ca6e16a2ce6eccfc8a6a65f
DavidKasprak <kasprakdavid@gmail.com>
2024-10-24 12:35:17 +0300

    HW-5 (DONE)

--- src/validators/post.validator.tsx
+++ src/validators/post.validator.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import Joi from "joi";
 
-const PostValidator = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+export const postValidator = Joi.object({
+    title: Joi.string()
+        .pattern(/\w{3,}/)
+        .messages({
+            "string.pattern.base": "you don't match the pattern",
+        }),
+    body: Joi.string().min(3).messages({
+        "string.min": "age must be at least 1"}),
+    userId: Joi.number().min(1).messages({
+        "NUmber.min": "must be at least 1",
+    }),
+});
 
-export default PostValidator;
+export default postValidator;
