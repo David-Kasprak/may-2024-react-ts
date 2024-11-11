308248d678fb9462457ad9f5223e41862c05f5f7
DavidKasprak <kasprakdavid@gmail.com>
2024-11-11 13:45:00 +0200

    HW-9 (still in progress, v1)

--- src/components/menu/MenuComponent.tsx
+++ src/components/menu/MenuComponent.tsx
@@ -5,6 +5,7 @@ const MenuComponent = () => {
     return (
         <div>
             <ul>
+                <li><Link to={'/'}>Home Page</Link></li>
                 <li><Link to={'users'}>Users Page</Link></li>
                 <li><Link to={'posts'}>Posts Page</Link></li>
                 <li><Link to={'comments'}>Comments Page</Link></li>
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
+import {RouterProvider} from "react-router-dom";
+import {routes} from "./routes/routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <App />
+        {/*todo - delete the line below when certain that is is not required*/}
+        {/*<App />*/}
+        <RouterProvider router={routes}/>
     </Provider>
 );
