9023a4dcbfbe00b2f166b2510d020d6714376756
DavidKasprak <kasprakdavid@gmail.com>
2024-11-11 13:38:21 +0200

    HW-9 (still in progress)

---
+++ src/components/menu/MenuComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const MenuComponent = () => {
+    return (
+        <div>
+            <ul>
+                <li><Link to={'users'}>Users Page</Link></li>
+                <li><Link to={'posts'}>Posts Page</Link></li>
+                <li><Link to={'comments'}>Comments Page</Link></li>
+            </ul>
+        </div>
+    );
+};
+
+export default MenuComponent;
